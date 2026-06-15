95abaf27e8d18303833f5dd4cab7541a1954220e
bkmzDJ <yugov2008@gmail.com>
2026-06-15 22:59:22 +0500

    начал сайт делать, нужно улучшить качество фото и оптимизировать

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,17 @@
-//JAVA SCRIPT CODE:
+let bool = false
+let nav = document.getElementById('nav')
+let burger = document.getElementById('burger')
+document.getElementById('burger').addEventListener('click', ()=>{
+    if(!bool){
+        burger.style.rotate = "90deg"
+        burger.style.height = "5vh"
+        nav.style.right = "0"
+        bool = true
+    } else if (bool){
+        burger.style.rotate = "0deg"
+        burger.style.height = "4vh"
+        nav.style.right = "-30vw"
+        bool = false
+    }
+     
+})
