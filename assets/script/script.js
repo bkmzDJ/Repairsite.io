bd251271147d39b87ef2f4d8ee26ebcb2cfed723
bkmzDJ <yugov2008@gmail.com>
2026-06-19 18:20:44 +0500

    сделал блок услуги, делать отзывы

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,5 +13,35 @@ document.getElementById('burger').addEventListener('click', ()=>{
         nav.style.right = "-30vw"
         bool = false
     }
-     
 })
+let model_triger = document.getElementById('model')
+let service_triger = document.getElementById('service')
+function EventCalculate(){
+    let model = parseInt(document.getElementById('model').value);
+    let service = parseInt(document.getElementById('service').value);
+    let time
+    switch (service) {
+        case 600:
+            time = "5 мин"
+            break;
+        case 1200:
+            time = "20-30 мин"
+            break;
+        case 2000:
+            time = "1-3 дня"
+            break;
+        case 1800:
+            time = "1-2 часа"
+            break;
+        case 3500:
+            time = "1-2 дня"
+            break;
+        case 1500:
+            time = "1-2 часа"
+            break;
+    }
+    document.getElementById('price').textContent = model+service+" ₽";
+    document.getElementById('time').textContent = time
+}
+model_triger.addEventListener('change', EventCalculate)
+service_triger.addEventListener('change', EventCalculate)
