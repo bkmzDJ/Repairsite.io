966153c43b627d8c3672779fadac3136cd035264
bkmzDJ <yugov2008@gmail.com>
2026-06-23 21:48:28 +0500

    все готово

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,7 @@
 let bool = false
 let nav = document.getElementById('nav')
 let burger = document.getElementById('burger')
+
 document.getElementById('burger').addEventListener('click', ()=>{
     if(!bool){
         burger.style.rotate = "90deg"
@@ -10,7 +11,12 @@ document.getElementById('burger').addEventListener('click', ()=>{
     } else if (bool){
         burger.style.rotate = "0deg"
         burger.style.height = "4vh"
-        nav.style.right = "-30vw"
+        if (window.innerWidth < 650){
+            nav.style.right = "-60vw"
+        }else{
+            nav.style.right = "-30vw"
+        }
+        
         bool = false
     }
 })
